8f1bdb4bf31047b01d0657d0c49c9a510494638d
Geldun98 <geldun98@gmail.com>
2022-04-09 09:57:36 +0700

    SEO

---
+++ dist/_nuxt/757f065.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{224:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},c=t(51),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  This is dunghiki04101998\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
